df00e541e3507fcdc06a6e993b5812572669f490
Dinesh Udayan <dinesh1599@gmail.com>
2025-02-15 11:57:56 -0600

    created dialogues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
-
+import genie from './assets/Norm-profileinfobox.webp'
+import dialogue from './assets/dialogue.json'
 function App() {
+
+  const [finput, setFinput] = useState(0)
+
+  const checkValue = ()=>{
+    if(finput==null || finput ==0 )
+      {
+      console.log({dialogue})
+      alert("nothing")
+    }else{
+      const number  = finput
+
+    }
+  }
+  
+  const handleKeyDown = (e)=>{
+    if(e.key == 'Enter'){
+      return checkValue()
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='frame'>
+      <h1>THE SMARTEST GENIE EVER</h1>
+      <h2>{`Ahhh, you’ve summoned me, the Great Genie of Useless Mind-Reading! Think of a number—wait, no, just type it. I’ll ‘magically’ guess it with my unmatched mystical powers.`}</h2>
+      <img src={genie} alt='genie'/>
+      <p>Ok mortal, think of number by typing it in!</p>
+      <input type="number" 
+        value={finput} 
+        placeholder='Dont tell me your number, just type it' 
+        onChange={e=>setFinput(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={checkValue}>Click me</button>
     </div>
   );
 }
